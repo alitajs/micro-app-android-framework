09f13e418ee28034699013413c10ea8a0c330b20
张智强 <15080495660@dayu.work>
2021-01-16 15:44:15 +0800

    【修改】 1. 定位改高德定位

--- miniapp/src/main/java/com/alitajs/micro/ui/bridge/LocationAlitaBridge.java
+++ miniapp/src/main/java/com/alitajs/micro/ui/bridge/LocationAlitaBridge.java
@@ -1,42 +1,58 @@
 package com.alitajs.micro.ui.bridge;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
-import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.webkit.JavascriptInterface;
 
 import com.alitajs.micro.bean.CompletionBean;
-import com.alitajs.micro.data.ConstantValue;
 import com.alitajs.micro.ui.activity.BaseMiniActivity;
 import com.alitajs.micro.ui.web.CompletionHandler;
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 
 import org.json.JSONObject;
 
-public class LocationAlitaBridge {
+public class LocationAlitaBridge implements AMapLocationListener {
 
     BaseMiniActivity mActivity;
     CompletionHandler mHandler;
 
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient = null;
+
     public <T extends BaseMiniActivity> LocationAlitaBridge(T activity) {
         this.mActivity = activity;
+        //初始化定位
+        mLocationClient = new AMapLocationClient(activity);
+        //设置定位回调监听
+        mLocationClient.setLocationListener(this);
+        AMapLocationClientOption option = new AMapLocationClientOption();
+        /**
+         * 设置定位场景，目前支持三种场景（签到、出行、运动，默认无场景）
+         */
+        option.setLocationPurpose(AMapLocationClientOption.AMapLocationPurpose.SignIn);
+        //设置定位模式为AMapLocationMode.Hight_Accuracy，高精度模式。
+        option.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //设置是否返回地址信息（默认返回地址信息）
+        option.setNeedAddress(true);
+        mLocationClient.setLocationOption(option);
     }
 
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (resultCode == Activity.RESULT_OK) {
-            if (data != null) {
-                if (ConstantValue.OPEN_GPS_REQ_CODE == requestCode) {
-                    location();
-                }
+        /*if (resultCode == 0) {
+            if (ConstantValue.OPEN_GPS_REQ_CODE == requestCode) {
+                location();
             }
-        }
+        }*/
     }
 
     /**
@@ -48,12 +64,11 @@ public class LocationAlitaBridge {
     @JavascriptInterface
     public void getLocation(Object params, final CompletionHandler handler) {
         this.mHandler = handler;
-        mActivity.requestPermission(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                        Manifest.permission.READ_PHONE_STATE, Manifest.permission.ACCESS_FINE_LOCATION},
+        mActivity.requestPermission(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
                 new BaseMiniActivity.OnRequestPermissionListen() {
                     @Override
                     public void succeed() {
-                        if (ActivityCompat.checkSelfPermission(mActivity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                       /* if (ActivityCompat.checkSelfPermission(mActivity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                                 && ActivityCompat.checkSelfPermission(mActivity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                             return;
                         }
@@ -66,12 +81,17 @@ public class LocationAlitaBridge {
                             mActivity.startActivityForResult(intent, ConstantValue.OPEN_GPS_REQ_CODE);
                             return;
                         }
-                        location();
+                        location();*/
+                        if(null != mLocationClient){
+                            //设置场景模式后最好调用一次stop，再调用start以保证场景模式生效
+                            mLocationClient.stopLocation();
+                            mLocationClient.startLocation();
+                        }
                     }
 
                     @Override
                     public void fail() {
-                        mHandler.complete(new CompletionBean(3, "GPS权限未打开", "").getResult());
+                        mHandler.complete(new CompletionBean(3, "定位权限未打开", "").getResult());
                     }
                 });
 
@@ -90,15 +110,15 @@ public class LocationAlitaBridge {
             // 设置定位精确度 Criteria.ACCURACY_COARSE比较粗略，Criteria.ACCURACY_FINE则比较精细
             criteria.setAccuracy(Criteria.ACCURACY_FINE);
             // 设置是否要求速度
-            criteria.setSpeedRequired(false);
+            criteria.setSpeedRequired(true);
             // 设置是否允许运营商收费
-            criteria.setCostAllowed(false);
+            criteria.setCostAllowed(true);
             // 设置是否需要方位信息
-            criteria.setBearingRequired(false);
+            criteria.setBearingRequired(true);
             // 设置是否需要海拔信息
-            criteria.setAltitudeRequired(false);
+            criteria.setAltitudeRequired(true);
             // 设置对电源的需求
-            criteria.setPowerRequirement(Criteria.POWER_LOW);
+            criteria.setPowerRequirement(Criteria.POWER_HIGH);
             // 为获取地理位置信息时设置查询条件
             String bestProvider = locationManager.getBestProvider(criteria, true);
             //从GPS获取最新的定位信息
@@ -111,8 +131,27 @@ public class LocationAlitaBridge {
             //获取经度、纬度、等属性值
             jsonObject.put("longitude", location.getLongitude());
             jsonObject.put("latitude", location.getLatitude());
-            mHandler.complete(new CompletionBean(0, "定位成功", location).getResult());
+            mHandler.complete(new CompletionBean(0, "定位成功", jsonObject).getResult());
         } catch (Exception e) {
+            e.printStackTrace();
+            mHandler.complete(new CompletionBean(3, e.toString(), "").getResult());
+        }
+    }
+
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        try {
+            if (aMapLocation == null) {
+                mHandler.complete(new CompletionBean(3, "定位失败", "").getResult());
+                return;
+            }
+            JSONObject jsonObject = new JSONObject();
+            //获取经度、纬度、地址
+            jsonObject.put("longitude", aMapLocation.getLongitude());
+            jsonObject.put("latitude", aMapLocation.getLatitude());
+            jsonObject.put("address", aMapLocation.getAddress());
+            mHandler.complete(new CompletionBean(0, "定位成功", jsonObject).getResult());
+        }catch (Exception e) {
             e.printStackTrace();
             mHandler.complete(new CompletionBean(3, e.toString(), "").getResult());
         }
